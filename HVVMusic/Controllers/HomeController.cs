9c7b94d487d1ed43a41328d9d7c2badb7a6b7c3a
ha van vu <62437220+HaVanVu1708@users.noreply.github.com>
2021-01-07 22:05:47 +0700

    task06

---
+++ HVVMusic/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using HVVMusic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HVVMusic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        //public IActionResult Index() => View(repository.Musics);
+
+        public ViewResult Index(int musicPage = 1)
+            => View(repository.Musics
+                .OrderBy(p => p.MusicID)
+            .Skip((musicPage - 1) * PageSize)
+            .Take(PageSize)
+            );
+
+        //localhost:5000/?musicPage=2
+    }
+}
